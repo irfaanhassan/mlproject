88319523bbad353130ce75e2a50318592385bbf7
irfaanhassan <irfaanhassan005@gmail.com>
2025-05-01 13:58:39 +0530

    hyper parameter training

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -4,8 +4,8 @@ from dataclasses import dataclass
 import numpy as np 
 import pandas as pd
 from sklearn.compose import ColumnTransformer
-from sklearn.impute import SimpleImputer
-from sklearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer#for missing values
+from sklearn.pipeline import Pipeline#step by step implementation of a seqquence of data transformation
 from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
@@ -14,17 +14,18 @@ import os
 
 from src.utils import save_object
 
-@dataclass
-class DataTransformationConfig:
+@dataclass#data class is used as decorator to create methods like ___init__
+class DataTransformationConfig:#input and output file path
     preprocessor_obj_file_path=os.path.join('artifacts',"proprocessor.pkl")
 
 class DataTransformation:
     def __init__(self):
         self.data_transformation_config=DataTransformationConfig()
 
-    def get_data_transformer_object(self):
+    def get_data_transformer_object(self):#object to transform the data
         '''
-        This function si responsible for data trnasformation
+        This function is
+          responsible for data trnasformation
         
         '''
         try:
@@ -103,7 +104,9 @@ class DataTransformation:
 
             train_arr = np.c_[
                 input_feature_train_arr, np.array(target_feature_train_df)
-            ]
+            ]#forming matric coolumn wise
+            #np.c_ is used to concatenate two arrays column wise
+            #np.r_ is used to concatenate two arrays row wise
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
             logging.info(f"Saved preprocessing object.")
@@ -113,7 +116,7 @@ class DataTransformation:
                 file_path=self.data_transformation_config.preprocessor_obj_file_path,
                 obj=preprocessing_obj
 
-            )
+            )#def save_object in utils.py
 
             return (
                 train_arr,
